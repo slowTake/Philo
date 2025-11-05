5eb16c5348fb720d483f6b14355207c5cd305313
pnurmi <pnurmi.zt@gmail.com>
2025-11-05 15:17:28 +0200

    end of day push

--- incls/philo.h
+++ incls/philo.h
@@ -15,7 +15,7 @@
 int		isdigit(int c);
 int		error_msg(t_error_type type);
 int		cleanup(void);
-int		input_validation(int argc, char **argv, t_data data);
+void	input_validation(int argc, char **argv, t_data data);
 void	parsing(char **argv, t_data data);
 
 #endif
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 10:26:56 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/11/05 13:58:21 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/11/05 15:06:38 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,18 @@ void	input_validation(int argc, char **argv, t_data data)
 	parsing(**argv, data);
 }
 
-void parsing(char **argv, t_data data)
+void	parsing(char **argv, t_data data)
 {
-	int i = 0;
 
-	while(argv[1][i])
-	{
-		
-	}
+		data->philos_num = str_int(argv[1][0]);
+		data->philos_num = str_int(argv[1][1]);
+		data->philos_num = str_int(argv[1][2]);
+		data->philos_num = str_int(argv[1][3]);
+}
 
+int str_int(char *str)
+{
+	
 }
 
 static int	ft_isdigit(int c)
