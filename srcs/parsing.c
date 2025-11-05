1d9982c8d1ba3b6340beef14256b59cba358b4bc
pnurmi <pnurmi.zt@gmail.com>
2025-11-05 14:00:07 +0200

    tidied up validation and parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,19 +6,39 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 10:26:56 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/11/03 12:29:59 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/11/05 13:58:21 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	parsing(int argc, char **argv, t_data data)
+void	input_validation(int argc, char **argv, t_data data)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 4 && argc != 5)
+		return (err_msg(E_NOT_DIGIT));
+	while (argv[i])
+	{
+		if (isdigit(argv[1][i]) != 1)
+			return (printf("Not a digit"));
+		if (argv[1][i] <= 1)
+			return (printf("Not Positive"));
+		i++;
+	}
+	parsing(**argv, data);
+}
+
+void parsing(char **argv, t_data data)
 {
 	int i = 0;
-	if (argc != 4 && argc != 5)
-		return (printf("Invalid arguement"));
-	if(isdigit(argv[1][i]) != 1)
-		return(printf("Not a digit"));
+
+	while(argv[1][i])
+	{
+		
+	}
+
 }
 
 static int	ft_isdigit(int c)
