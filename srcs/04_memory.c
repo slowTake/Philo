144d42fd2d1267e46ac9d3ce45dda542044f0bc5
pnurmi <pnurmi.zt@gmail.com>
2025-11-11 18:03:52 +0200

    updated and merged from remote

--- srcs/04_memory.c
+++ srcs/04_memory.c
@@ -2,10 +2,10 @@
 
 int	alloc_memory(t_data *data)
 {
-	data->philosophers = (t_philo *)malloc(sizeof(t_philo) * data->philos_num);
+	data->philosophers = (t_philo *)malloc(sizeof(t_philo) * data->philo_count);
 	if (data->philosophers == NULL)
 		return(err_msg("Phillo alloc failed"));
-	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)	* data->philos_num);
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)	* data->philo_count);
 	if (data->forks == NULL)
 	{
 		free(data->philosophers);
