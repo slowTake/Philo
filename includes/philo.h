2f6f765edc88e35d2ccb10ec344dc54d628685b5
pnurmi <pnurmi.zt@gmail.com>
2025-11-19 12:34:17 +0200

    first push of the day

--- includes/philo.h
+++ includes/philo.h
@@ -11,23 +11,21 @@
 // ours
 # include "structs.h"
 
-//01_parsing
+// 01_parsing
 int	parsing(int argc, char **argv, t_data data);
 int	isdigit(int c);
 int	input_validation(int argc, char **argv, t_data data);
 int	input_assignment(char **argv, t_data data);
 int	ft_strtol(const char *str);
 
-//05_error
+// 05_error
 int	err_invalid(void);
 int	err_msg(const char *str);
 int	error_msg(t_error_type type);
 
-//utility
+// utility
 
-
-
-//memory
-int alloc_memory(t_data *data);
+// memory
+int	alloc_memory(t_data *data);
 
 #endif
