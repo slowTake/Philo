07a0ae30d940d3ba611a3ea15e7b2cdd7a93b1c0
pnurmi <pnurmi.zt@gmail.com>
2026-01-27 09:41:16 +0200

    passing stress tests with changes to thinking time

--- srcs/10_routine.c
+++ srcs/10_routine.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:26:28 by pnurmi            #+#    #+#             */
-/*   Updated: 2026/01/25 17:20:44 by pnurmi           ###   ########.fr       */
+/*   Updated: 2026/01/27 09:40:19 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,90 +14,97 @@
 
 static int	eating(t_data *data)
 {
-	long	total_time;
-	int		i;
+	long		total_time;
+	long long	start_eat;
 
 	total_time = data->time_to_eat * 1000;
-	i = 0;
-	while (i < 3)
+	start_eat = get_current_time_ms();
+	while ((get_current_time_ms() - start_eat) < data->time_to_eat)
 	{
-		usleep(total_time / 3);
+		usleep(500);
 		if (simulation_finished(data))
 			return (1);
-		i++;
 	}
 	return (0);
 }
 
 static int	thinking(t_data *data, t_philo *philo)
 {
-	long	think_time;
-	int		i;
+	float		think_time;
+	long long	start_think;
+	long long	time_left;
 
-	think_time = ((get_current_time_ms() - philo->last_meal_time) / 2);
-	i = 0;
-	while (i < 5)
+	time_left = data->time_to_die - (get_current_time_ms()
+			- philo->last_meal_time - data->time_to_eat);
+	think_time = (data->time_to_die - data->time_to_eat - data->time_to_sleep);
+	if (data->philo_count % 2 == 1)
+		think_time = (think_time * 0.75);
+	else
+		think_time = think_time * 0.25;
+	if (think_time > time_left)
+		think_time = time_left;
+	start_think = get_current_time_ms();
+	while (get_current_time_ms() - start_think < think_time)
 	{
-		usleep(think_time / 5);
+		if (think_time > data->time_to_die)
+			return (1);
+		usleep(500);
 		if (simulation_finished(data))
 			return (1);
-		i++;
 	}
 	return (0);
 }
 
 static int	sleeping(t_data *data)
 {
-	long	sleep_time;
-	int		i;
+	long long	start_sleep;
 
-	sleep_time = data->time_to_sleep * 1000;
-	i = 0;
-	while (i < 3)
+	start_sleep = get_current_time_ms();
+	while ((get_current_time_ms() - start_sleep) < data->time_to_sleep)
 	{
-		usleep(sleep_time / 3);
+		usleep(500);
 		if (simulation_finished(data))
 			return (1);
-		i++;
 	}
 	return (0);
 }
 
-static void philo_loop(t_philo *philo)
+static void	philo_loop(t_philo *philo)
 {
 	while (simulation_finished(philo->data) == 0)
 	{
-		print_status(philo->data, philo->p_id, "is thinking");
-		if (thinking(philo->data, philo))
-			break ;
+		if (philo->meals_eaten != 0)
+		{
+			if (simulation_finished(philo->data) == 0)
+				print_status(philo->data, philo->p_id, "is thinking");
+			if (thinking(philo->data, philo))
+				break ;
+		}
 		if (!take_forks(philo))
 		{
 			update_last_meal_time(philo);
-			print_status(philo->data, philo->p_id, "is eating");
+			if (simulation_finished(philo->data) == 0)
+				print_status(philo->data, philo->p_id, "is eating");
 			if (eating(philo->data))
 			{
 				put_forks(philo);
 				break ;
 			}
 			put_forks(philo);
-			print_status(philo->data, philo->p_id, "is sleeping");
+			if (simulation_finished(philo->data) == 0)
+				print_status(philo->data, philo->p_id, "is sleeping");
 			sleeping(philo->data);
 		}
 	}
 }
 
-void *philo_routine(void *arg)
+void	*philo_routine(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	if (philo->p_id % 2 == 1)
-	{
-		if (philo->data->philo_count == philo->p_id)
-			usleep(1000);
-		else
-			usleep(5700);
-	}
+	if (philo->p_id % 2 == 0)
+		usleep(500);
 	philo_loop(philo);
 	return (NULL);
 }
